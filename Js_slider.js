c5f50bc68d5765d8ef391f80992007bdea28f2f4
AryanRajTalekar <156109321+AryanRajTalekar@users.noreply.github.com>
2024-08-28 12:56:02 +0530

    news section added

--- Js_slider.js
+++ Js_slider.js
@@ -1,35 +1,3 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('.main'),
-    smooth: true
-});
-
-
-function loadingAnimation(){
-    gsap.from("#heading1",{
-        y: 100,
-        opacity: 0,
-        delay: 1,
-        duration: 0.9,
-        stagger: 0.3
-    });
-    gsap.from("#heading2",{
-        y: 100,
-        opacity: 0,
-        delay: 2,
-        duration: 0.9,
-        stagger: 0.3
-    });
-    gsap.from("#heading3",{
-        y: 100,
-        opacity: 0,
-        delay: 3,
-        duration: 0.9,
-        stagger: 0.3
-    }); 
-}
-
-loadingAnimation();
-
 let currentSlide = 0;
 
 function showSlide(index) {
